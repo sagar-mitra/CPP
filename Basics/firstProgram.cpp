a52c0bb820c3f1dde6681d95e8a9fbbc54ad919e
Sagar Mitra <mitrasagar10@gmail.com>
2025-04-14 10:44:36 +0530

    first-commit

---
+++ Basics/firstProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello my name is Sagar Mitra";
+    cout << "\n";
+    cout << endl;
+    cout << "I am 23 years old";
+}
---
+++ Basics/numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "4" << endl; // 4 -- String
+    cout << 4 << endl; // 4 -- Number
+    
+    cout << "4+3" << endl; // 4+3 -- String
+    cout << 4+3 << endl; //7 -- Number
+}
